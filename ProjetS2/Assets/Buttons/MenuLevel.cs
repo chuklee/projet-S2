5f559178dbfb2652b71b1225282fe893ca4da5ba
PREVOT <baptiste.prevot@epita.fr>
2020-12-17 19:34:05 +0100

    FixBug

---
+++ ProjetS2/Assets/Buttons/MenuLevel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuLevel : MonoBehaviour
+{
+    public void LevelButtonScript()
+    {
+        SceneManager.LoadScene("LevelsMenu");
+    }
+
+}
+
---
+++ ProjetS2/Assets/Scripts/Level1Button.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Level1Button : MonoBehaviour
+{
+    public void LevelButtonScript()
+    {
+        SceneManager.LoadScene("Niveau1");
+    }
+
+}
