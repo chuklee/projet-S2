cd7cc82f6585a5f9831a43fd88db7453fd4c16e4
PREVOT <baptiste.prevot@epita.fr>
2020-12-17 15:57:02 +0100

    Jour2 Menu

---
+++ ProjetS2/Assets/Buttons/ToLevelMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ToLevelMenu : MonoBehaviour
+{
+    public void PlayButtonScript()
+    {
+        SceneManager.LoadScene("LevelsMenu");
+    }
+
+}
+
