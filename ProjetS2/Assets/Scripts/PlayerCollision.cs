efc6db7d60cef0ad873ea4791e9820907e568daa
nmarcsoft <n.rioumarc@gmail.com>
2021-01-27 23:11:35 +0100

    Premier Trap
    Detection et affichage message de mort sur la Console.

---
+++ ProjetS2/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public int lifePoints = 100;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Trap")
+        {
+            lifePoints = 0;
+            print("Dead");
+        }
+        else
+        {
+            print("Loop");
+        }
+    }
+}
--- ProjetS2/Assets/Scripts/PlayerController.cs
+++ ProjetS2/Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 //A FAIRE : -Regarder ce que c'est un Vecteur3
 //          - Regarder ce que ça veut dire normaliser des données
@@ -25,9 +26,14 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        
         //Calculer la vélocité (vitesse) du mouvement de notre joueur
-        float xMov = Input.GetAxisRaw("Horizontal");//Raw car on ne veux pas filtre. Récupère les mouvements du joueur horizontale (compris entre -1;1)
-        float zMov = Input.GetAxisRaw("Vertical");//Récupère les mouvements du joueur en verticale (compris entre -1;1)
+        float
+            xMov = Input.GetAxisRaw(
+                "Horizontal"); //Raw car on ne veux pas filtre. Récupère les mouvements du joueur horizontale (compris entre -1;1)
+        float
+            zMov = Input
+                .GetAxisRaw("Vertical"); //Récupère les mouvements du joueur en verticale (compris entre -1;1)
 
         Vector3 moveHorizontal = transform.right * xMov;
         Vector3 moveVertical = transform.forward * zMov;
@@ -37,19 +43,18 @@ public class PlayerController : MonoBehaviour
         motor.Move(velocity);
 
         //On calcule la rotation du joeur en un Vector3
-        float yRot = Input.GetAxisRaw("Mouse X");// Récupére le X de la souris
+        float yRot = Input.GetAxisRaw("Mouse X"); // Récupére le X de la souris
 
         Vector3 rotation = new Vector3(0, yRot, 0) * mouseSensitivityX;
 
         motor.Rotate(rotation);
 
         //On calcule la rotation de la caméra (afin que le joueur ne soit pas incliné lors de ses déplacements de souris) en un Vector3
-        float xRot = Input.GetAxisRaw("Mouse Y");// Récupére le X de la souris
+        float xRot = Input.GetAxisRaw("Mouse Y"); // Récupére le X de la souris
 
         Vector3 cameraRotation = new Vector3(xRot, 0, 0) * mouseSensitivityY;
 
-        motor.RotateCamera(cameraRotation);//Mettre moins si on veux inverser la caméra
-
+        motor.RotateCamera(cameraRotation); //Mettre moins si on veux inverser la caméra
     }
 
 }
