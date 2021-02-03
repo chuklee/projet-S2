0391b30ab58b9832b4cadd9e288e62e5da6dc787
Martin <martinnatale@mail.com>
2021-02-03 19:06:08 +0100

    Réparation de la branche Mirror

---
+++ ProjetS2/Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using Mirror;
+
+public class PlayerSetup : NetworkBehaviour //On change ça car MonoBehaviour ne sert que qu'en le script est pour la solo
+{
+    [SerializeField]
+    Behaviour[] componentsToDisable;
+
+    Camera sceneCamera;
+    private void Start()
+    {
+        if (!isLocalPlayer)//Regarde si le player est bien nous
+        {
+            //On va boucler sur les différents composants renseignés et les désactiver si ce joueur n'est pas le notre
+            for (int i = 0; i < componentsToDisable.Length; i++)
+            {
+                componentsToDisable[i].enabled = false;// On désactive chaque component
+            }
+        }
+        else
+        {
+            sceneCamera = Camera.main;
+            if(sceneCamera != null)
+            {
+                sceneCamera.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void OnDisable()
+    {
+        if(sceneCamera != null)
+        {
+            sceneCamera.gameObject.SetActive(true);
+        }
+    }
+}
