7dc839a67a561e5f609d971d0dc41df203c14dbd
Martin <martinnatale@mail.com>
2021-02-03 19:31:37 +0100

    Manual merge Mirror into Main

---
+++ ProjetS2/Assets/Buttons/Level1.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Level1 : MonoBehaviour
+{
+    public void PlayButtonScript()
+    {
+        SceneManager.LoadScene("Niveau1");
+    }
+
+}
